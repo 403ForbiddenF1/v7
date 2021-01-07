e0f1ee8086d6cb9a72e04dc64c764eb4e433ebb1
F1001R <75436003+F1001R@users.noreply.github.com>
2021-01-07 13:53:50 -0300

    Add files via upload

---
+++ necessario.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "
+=========================================
+ ▀ █▄░█ ▄▀▀ ▀█▀ ▄▀▄ █░░ ▄▀▄ █▄░█ █▀▄ ▄▀▄|
+ █ █░▀█ ░▀▄ ░█░ █▀█ █░▄ █▀█ █░▀█ █░█ █░█|
+ ▀ ▀░░▀ ▀▀░ ░▀░ ▀░▀ ▀▀▀ ▀░▀ ▀░░▀ ▀▀░ ░▀░|
+                           ==============
+ █▀▄ ▄▀▄ ▄▀ ▄▀▄ ▀█▀ █▀▀ ▄▀▀|
+ █░█ █▀█ █░ █░█ ░█░ █▀▀ ░▀▄|
+ █▀░ ▀░▀ ░▀ ░▀░ ░▀░ ▀▀▀ ▀▀░|
+                           ================
+ █▄░█ █▀▀ ▄▀ █▀▀ ▄▀▀ ▄▀▀ ▄▀▄ █▀▀▄ ▀ ▄▀▄ ▄▀▀|
+ █░▀█ █▀▀ █░ █▀▀ ░▀▄ ░▀▄ █▀█ █▐█▀ █ █░█ ░▀▄|
+ ▀░░▀ ▀▀▀ ░▀ ▀▀▀ ▀▀░ ▀▀░ ▀░▀ ▀░▀▀ ▀ ░▀░ ▀▀░|
+============================================
+"
+pkg update && pkg upgrade
+
+pkg install nano
+
+pkg install php -y
+
+pkg install git -y
+
+pkg install bash
+
+pkg install clang -y
+
+pkg install python -y
+
+pkg install python2
+
+pkg install python3
+
+pip install requests
+
+pip install --upgrade pip
+
+pip install bs4
+
+pip install rsa
+
+pip install asyncio
+
+pip install wheel
+
+pip install colorama
+
+pip install pytz
+
+pip install pyaes
+
+pip install async_generator
+
+pip install telethon
+
+pip install -r requirements
+
+pip install toilet -y
+
+pip install figlet
+
+pkg install ruby -y
+
+gem install lolcat
+
+echo "
+
+ ▀█▀ █░█ █▀▄ ▄▀▄
+ ░█░ █░█ █░█ █░█
+ ░▀░ ░▀░ ▀▀░ ░▀░
+
+ ▀ █▄░█ ▄▀▀ ▀█▀ ▄▀▄ █░░ ▄▀▄ █▀▄ ▄▀▄
+ █ █░▀█ ░▀▄ ░█░ █▀█ █░▄ █▀█ █░█ █░█
+ ▀ ▀░░▀ ▀▀░ ░▀░ ▀░▀ ▀▀▀ ▀░▀ ▀▀░ ░▀░
+"
+
+echo "
+
+DIGITE AGORA python v7.py
+SE JA EDITOU OS ARQUIVOS Numbers.txt e o wd.json
+"
